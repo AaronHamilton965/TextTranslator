4df0c6345e6398da7002ec803789b671599f054d
Kyle <kyleedwardgjava@gmail.com>
2021-10-11 18:22:14 -0700

    New ToDos

--- api.py
+++ api.py
@@ -1,5 +1,8 @@
 import json, sys
 import requests
+
+
+# TODO: ReDo function in order to accomdate different Languages
 def getTranslation(text):
 
     url = "https://translate.astian.org/translate"
